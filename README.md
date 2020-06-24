d8646ac5fff25c88863bec9c6b6a7dc6c7b26ad5
Romain Gonord <romain.gonord.opensource@neteyes.org>
2020-06-24 11:28:24 +0200

    run proxy on custom port and domain in order to avoid priviledge port while developing

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ npm install
 To start:
 
 ```sh
-sudo REMOTE_SERVER_DOMAIN=scandipwapmrev.indvp.com node index.js
+REMOTE_SERVER_DOMAIN=scandipwapmrev.indvp.com LOCAL_PORT=3000 LOCAL_DOMAIN=scandipwa node index.js
+# application is available on http://scandipwa:3000
 ```
 
 For the REMOTE_SERVER_DOMAIN environment variable, you can set whatever domain you want as long as there is a Magento instance behind it.
--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import httpProxyMiddleware from "http-proxy-middleware";
 const { createProxyMiddleware } = httpProxyMiddleware;
 const REMOTE_SERVER_DOMAIN = process.env.REMOTE_SERVER_DOMAIN;
 
-const pwaProxy = createProxyMiddleware({ target: `http://localhost:3003`, ws: true });
-const magentoProxy = createProxyMiddleware({ target: `http://${REMOTE_SERVER_DOMAIN}`, logLevel: "debug" });
+const pwaProxy = createProxyMiddleware({
+  target: `http://localhost:3003`,
+  ws: true,
+});
+const magentoProxy = createProxyMiddleware({
+  target: `http://${REMOTE_SERVER_DOMAIN}`,
+  logLevel: "debug",
+});
 
 const app = express();
 app.use("/graphql", magentoProxy);
@@ -14,10 +20,12 @@ app.use("/media", magentoProxy);
 app.use("/admin", magentoProxy);
 app.use("/", pwaProxy);
 
-const hostname = "0.0.0.0";
-const port = 80;
-app.listen(port, hostname, function () {
+const hostname = process.env.LOCAL_DOMAIN || "0.0.0.0";
+const port = process.env.LOCAL_PORT || 8044;
+app
+  .listen(port, hostname, function () {
     console.log(`Server is listening on http://${hostname}:${port}`);
-}).on("error", (err) => {
+  })
+  .on("error", (err) => {
     console.error("Error when starting reverse-proxy", err);
-});
+  });
